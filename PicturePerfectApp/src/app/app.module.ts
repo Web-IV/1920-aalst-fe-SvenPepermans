3332575b522e4b576dcaa754c3db75dfffcffd71
Sven <sven.pepermans@student.hogent.be>
2020-05-09 21:18:02 +0200

    Changes ivm upload functie
    Fotos worden opgeslaan in db MAAR worden niet naar object gemapt omdat Foto.FromJSON = null

--- PicturePerfectApp/src/app/app.module.ts
+++ PicturePerfectApp/src/app/app.module.ts
@@ -1,13 +1,12 @@
+import { MatInputModule } from '@angular/material/input';
 import { PostDetailComponent } from './post/post-detail/post-detail.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { AddPostComponent } from './post/add-post/add-post.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PostModule } from './post/post.module';
 import { MaterialModule } from './material/material.module';
-
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,17 +14,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule, Routes } from '@angular/router';
-
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 
 const appRoutes: Routes = [
   { path: 'post/home', component: PostListComponent },
   { path: 'post/add', component: AddPostComponent },
-  { path: 'post/detail/:id', component: PostDetailComponent},
+  { path: 'post/detail/:id', component: PostDetailComponent },
   { path: '', redirectTo: 'post/all', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent}
-
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -40,6 +37,7 @@ const appRoutes: Routes = [
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatInputModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- PicturePerfectApp/src/app/post/add-post/add-post.component.ts
+++ PicturePerfectApp/src/app/post/add-post/add-post.component.ts
@@ -1,3 +1,4 @@
+import { Foto } from './../foto.model';
 import { HttpClient, HttpEventType } from '@angular/common/http';
 
 import { Post } from './../post.model';
@@ -6,9 +7,11 @@ import {
   FormControl,
   FormGroup,
   FormBuilder,
-  Validators
+  Validators,
+  FormArray
 } from '@angular/forms';
 import { PostDataService } from '../post-data.service';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-post',
@@ -19,24 +22,65 @@ export class AddPostComponent implements OnInit {
   public post: FormGroup;
   public progress: number;
   public message: string;
+  public returnData: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private _postDataService : PostDataService) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private _postDataService: PostDataService
+  ) {}
+
+  get fotos(): FormArray {
+    return <FormArray>this.post.get('fotos');
+  }
 
   ngOnInit(): void {
     this.post = this.fb.group({
-      beschrijving: ['', [Validators.required, Validators.minLength(10)]],
-      categorie: ['', [Validators.required]]
+      beschrijving: ['', [Validators.required, Validators.minLength(2)]],
+      categorie: ['', [Validators.required]],
+      fotos: this.fb.array([this.createFotos()])
+    });
+
+    this.fotos.valueChanges.pipe(distinctUntilChanged()).subscribe(fList => {
+      this.fotos.push(this.createFotos());
     });
   }
-  public uploadFile = files => {
-    if (files.length === 0) {
-      return;
-    }
 
+  onSubmit() {
+    let fotos = this.post.value.fotos.map(Foto.fromJSON);
+    this._postDataService.addNewPost(
+      new Post(this.post.value.beschrijving, this.post.value.categorie, fotos)
+    );
+    this.post = this.fb.group({
+      beschrijving: ['', [Validators.required, Validators.minLength(2)]],
+      categorie: ['', [Validators.required]],
+      fotos: this.fb.array([this.createFotos()])
+    });
+  }
+
+  getErrorMessage(errors: any): string {
+    if (errors.required) {
+      return 'is required';
+    } else if (errors.minlength) {
+      return `needs at least ${errors.minlength.requiredLength} charactes (got ${errors.minlength.actualLength})`;
+    }
+  }
+
+  createFotos(): FormGroup {
+    //////////Foto omzetten naar klasse//////////
+    return this.returnData;
+  }
+
+  uploadFotos(files: any): void {
+    ///////File uploaden naar DB////////
     let filesToUpload: File[] = files;
     const formData = new FormData();
 
     Array.from(filesToUpload).map((file, index) => {
+      this.returnData = this.fb.group({
+        name: [file.name],
+        url: ['een url']
+      });
       return formData.append('file' + index, file, file.name);
     });
 
@@ -50,32 +94,7 @@ export class AddPostComponent implements OnInit {
           this.progress = Math.round((100 * event.loaded) / event.total);
         else if (event.type === HttpEventType.Response) {
           this.message = 'Upload success.';
-         
         }
-      });    
-  };
-
-  onSubmit() {
-    this.post = this.fb.group({
-      beschrijving: ['', [Validators.required, Validators.minLength(10)]],
-      categorie: ['', [Validators.required]]
-    });
-    var newPost =  new Post(this.post.value.beschrijving, this.post.value.categorie)
-    this._postDataService.addNewPost(
-     newPost
-     
-    );
-    
-    
+      });
   }
-
-  getErrorMessage(errors: any): string {
-    if (errors.required) {
-      return 'is required';
-    } else if (errors.minlength) {
-      return `needs at least ${errors.minlength.requiredLength} charactes (got ${errors.minlength.actualLength})`;
-    }
-  }
-
-
 }
