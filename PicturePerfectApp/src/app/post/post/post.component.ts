c75e7dca6fa1021cf6ba9f30ab21d1994b9de6d2
Sven <sven.pepermans@student.hogent.be>
2020-05-22 17:46:08 +0200

    fixed edit, delite en like knop zodat eigenaar van post enkel kan editen en deleten, en niet kan liken, vice versa voor mensen die jouw posts bekijken

--- PicturePerfectApp/src/app/post/post/post.component.ts
+++ PicturePerfectApp/src/app/post/post/post.component.ts
@@ -1,9 +1,12 @@
+import { map } from 'rxjs/operators';
 import { PostDataService } from './../post-data.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { UserDataService } from '../../user/user-data.service';
+import { User } from '../../user/user.model';
+
 
 @Component({
   selector: 'app-post',
@@ -13,8 +16,9 @@ import { UserDataService } from '../../user/user-data.service';
 export class PostComponent implements OnInit {
   @Input() public post: Post;
   likeButtonDisabled: boolean = false;
+  currentUserId: number;
   images: any[];
-  loggedInUser$ = this._userDataService.getCurrentUser$();
+  
   constructor(
     private sanitizer: DomSanitizer,
     private _postDataService: PostDataService,
@@ -25,6 +29,7 @@ export class PostComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.post);
     this.images = this.post.fotos.map(foto => foto.base64);
+    
   }
 
   sanitize(url: string) {
@@ -43,5 +48,17 @@ export class PostComponent implements OnInit {
     console.log(this.post);
     this.likeButtonDisabled = true;
     this._postDataService.editPost(this.post);
+  }
+
+  compareUsers(): boolean{
+    var returnvalue;
+    this._userDataService.getCurrentUser$().pipe(
+      map(user => this.currentUserId = user.gebruikersId))
+      
+    
+    returnvalue = this.currentUserId == this.post.gebruiker.gebruikersId ;
+ 
+  console.log(returnvalue)
+  return returnvalue;
   }
 }
